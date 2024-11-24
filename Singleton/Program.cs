0c28ee20618b3429090dbc7ced3f598e603fa553
cacuci <rafaelcacuci@yahoo.com.br>
2024-11-24 11:32:28 -0300

    Revisited pattern Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // O método Instance sempre retornará a mesma instacia do objeto              
+            var instance = Singleton.Instance();
+
+            // O método Instance sempre retornará a mesma instacia do objeto              
+            var instance2 = Singleton.Instance();
+
+            // Aqui é possivel pereceber que objetos são identicos, ou seja, a instancia é a mesma              
+            Console.WriteLine(instance.Equals(instance2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+namespace Singleton
+{
+    // A classe Singleton define o método 'Instance' que permite
+    // clientes acessar a mesma instância de um DataTable.
+    internal class Singleton
+    {
+        // O campo para armazenar a instância singleton deve ser
+        // declarado como estático
+        private static readonly DataTable instance = new();
+
+        // O construtor do singleton devem sempre ser privado para
+        // prevenir chamadas diretas de construção com o operador
+        // `new`.
+        private Singleton() { }
+
+        // O método estático que controla acesso à instância do
+        // singleton
+        public static DataTable Instance()
+        {
+            return instance;
+        }
+    }
+}
